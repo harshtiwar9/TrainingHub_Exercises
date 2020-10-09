8a4dc434662e9258e9a5bfcd688cdf2736b04c1f
Harsh Tiwar <harsh.tiwar9@gmail.com>
2020-10-09 14:15:47 -0400

    Exercise-4 Ques.2 Done

--- Exercise-4(JS)/Exercise-4.js
+++ Exercise-4(JS)/Exercise-4.js
@@ -2,24 +2,31 @@
 
 console.log("\nAnswer-1: In JavaScript, the “number” type cannot represent integer values larger than (253-1) (that’s 9007199254740991),\n or less than -(253-1) for negatives.");
 console.log("The 'n' at the end means it's a BigInt");
-console.log("typeof 10n: " + typeof 10n);
+//console.log("typeof 10n: " + typeof 10n);
+console.log("typeof 10n: " + typeof BigInt(10));
 
 // Use the inbuilt js array functions to perform the below tasks
 // 2)write the implementations function of the filter function just like the map one we did in the class
 
 console.log("\nAnswer-2:")
 const arr = [1, 2, 3, 4, 5, 6];
-console.log(arr.map(elm => elm * elm));
+console.log(arr.filter((elm) => elm % 2 == 0).map(function(elm) { return elm; })); //return only if element is Even.
+console.log(arr.map((elm) => elm % 2 == 0 ? "Even" : "odd")); //return if no is odd or even from array.
 
-// 3) const pets = ['cat', 'dog', 'bat'];
-// function checkIfFound(){
-//   ... implement your code
-// }
-// Input : checkIfFound("cat")
-// expected output: display "found"
+// 3) 
+const pets = ['cat', 'dog', 'bat'];
 
-// Input : checkIfFound("elephant")
-// expected output: display "Not found"
+function checkIfFound(pet) {
+    //... implement your code
+    console.log(pets.map((elm) => elm === pet ? "Found!" : "Not Found!"));
+    //console.log(pets.filter((elm) => elm === pet).map(function(elm) { if (elm.length > 0) { return "Found!" } else { return "Not Found!"; } }));
+    console.log(pets.filter((elm) => elm === pet).map(elm => { if (elm.length > 0) { return "Found!" } else { return "Not Found!"; } }));
+}
+checkIfFound("cat")
+    //expected output: display "found"
+
+checkIfFound("elephant")
+    //expected output: display "Not found"
 
 // 5) const elements = ['Fire', 'Air', 'Water'];
 // function concateWithComma(){
